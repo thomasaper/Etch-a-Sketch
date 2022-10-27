e257f23021f62932e38163c67e7cab29412b1905
Thomas Aper <thomasaper@gmx.de>
2022-10-27 16:44:32 +0200

    created Grid 16x16

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 
-// Create a webpage with a 16x16 grid of square divs
+// Creating a webpage with a 16x16 grid of square divs
 
-for (let i=0; i<=255; i++) {
 
+const row = document.querySelector('#row');
+const column = document.querySelector('#column');
+
+
+
+function makeRow(n) {
+
+    for (let c=0; c<=n; c++){
+    for (let r=0; r<=n; r++){
+        const squareDiv = document.createElement('div');
+        squareDiv.classList.add('squareDiv');
+        row.appendChild(squareDiv);
+    }
 };
+};
+
+
+
+makeRow(16);
