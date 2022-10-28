02c154e993dd54fa88ce0af95a5d9bafbfa9d1d4
Thomas Aper <thomasaper@gmx.de>
2022-10-28 16:22:10 +0200

    some changes

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 
 // Creating a webpage with a 16x16 grid of square divs
 
+const body = document.querySelector('body'); // Reference to body
 
-const row = document.querySelector('#row');
-const column = document.querySelector('#column');
+const grid = document.createElement('div');  // Creating div for the grid
+grid.classList.add('grid'); // adding class grid
+body.appendChild(grid); // appending div for grid to body
+
+const rows = document.createElement('div'); // Creating div for rows 
+rows.classList.add('rows'); // adding class rows
+grid.appendChild(rows); // appending div for rows to grid
+
+const singleRow = document.createElement('div'); // Creating div for singleRow
+singleRow.classList.add('singleRow'); // adding class singeRow
+rows.appendChild(singleRow); // appending div singleRow to rows
 
 
 
-function makeRow(n) {
+const makeSingleRow = function(n) {
 
-    for (let c=0; c<=n; c++){
-    for (let r=0; r<=n; r++){
-        const squareDiv = document.createElement('div');
-        squareDiv.classList.add('squareDiv');
-        row.appendChild(squareDiv);
+    for (let s=0; s<n; s++) {
+        const cell = document.createElement('div'); // Creating div for cells
+        cell.classList.add('cell'); // adding class cell
+        singleRow.appendChild(cell); // appending div cell to singleRow
     }
 };
+
+
+function makeRows(n) {
+
+    for (let r=0; r<n; r++) {
+        //makeSingleRow(n);
+        
+    }
 };
 
 
 
-makeRow(16);
+    makeSingleRow(16)
+    makeRows(16);
+
+
+
+
+
