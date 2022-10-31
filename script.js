61ca40dc09d93db40ca57c7d5b894b4cd5e59d3e
Thomas Aper <thomasaper@gmx.de>
2022-10-31 10:06:34 +0100

    change color when mouseover

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+///// Create a webpage with a 16x16 grid of square divs. /////
+
+
+// Important consts for grid
 const container = document.querySelector("#container"); // Reference to container
 const rows = document.getElementsByClassName("gridRow"); // Reference to gridRow
-const cells = document.getElementsByClassName("cell"); // Reference to cell
+const cell = document.getElementsByClassName("cell"); // Reference to cell
 
 // Creates a default grid sized n
 function defaultGrid(n) {
     makeRows(n);
     makeCells(n);
-}
+};
 
 // Creates rows
 function makeRows(rowNum) {
@@ -26,4 +30,21 @@ function makeCells(cellNum) {
     };
 };
 
-defaultGrid(16);
+// calling the function
+defaultGrid(500);
+
+
+
+//change color when mouseover 
+
+function changeColor(target) {
+    target.style.backgroundColor = "black"; 
+}
+
+container.addEventListener('mouseover', function (e) {
+    target = e.target;
+
+    if(target.matches("div.cell")){
+        changeColor(target);
+    }
+  });
